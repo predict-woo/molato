972302671bef07146ae07e66abf7753273118676
Nicole Lee <nicolelee2001@gmail.com>
2024-03-23 17:59:27 +0900

    create molato select page

---
+++ src/page/Present/atoms/MainText.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const MainText = styled.p`
+  display: inline-block;
+  color: var(--black, #333);
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 28px;
+  margin: 0;
+  padding: 0;
+`;
+
+export const HighlightedMainText = styled.p`
+  display: inline-block;
+  color: var(--primary, #d25151);
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 28px;
+  margin: 0;
+  padding: 0;
+`;
